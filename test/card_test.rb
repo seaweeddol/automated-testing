694e05a8f2bf59739584ad418a77d1979dc2e7c0
Jessica Stone <jstone4815@gmail.com>
2020-02-24 12:32:24 -0800

    write tests for waves 1 and 2

--- test/card_test.rb
+++ test/card_test.rb
@@ -18,6 +18,8 @@ describe Card do
       expect {Card.new(0, :diamonds)}.must_raise ArgumentError
       expect {Card.new(14, :diamonds)}.must_raise ArgumentError
       expect {Card.new(14, :clovers)}.must_raise ArgumentError
+      expect {Card.new(5, :bananas)}.must_raise ArgumentError
+
     end
   end
 
@@ -26,6 +28,13 @@ describe Card do
     it "to_s returns a readable String value logically for values 2-10" do
       # Test to ensure that to_s works for cards values 2-10
       # for example:  "2 of diamonds"
+      [:hearts, :spades, :clubs, :diamonds].each do |suit|
+        (2..10).each do |value|
+          card = Card.new(value, suit)
+          expect(card.to_s).must_equal "#{value} of #{suit}"
+        end
+      end
+
     end
 
     it "to_s returns a readable String value for Ace, Jack, Queen, King" do
@@ -34,6 +43,22 @@ describe Card do
       # The current implementation of to_s does not address this feature
       # Write the test, see it fail, then modify to_s to make it pass!
       # (Consider writing a helper method!)
+      [:hearts, :spades, :clubs, :diamonds].each do |suit|
+        [1, 11, 12, 13].each do |value|
+          card = Card.new(value, suit)
+          case value 
+            when 1
+              expect(card.to_s).must_equal "Ace of #{suit}"
+            when 11
+              expect(card.to_s).must_equal "Jack of #{suit}"
+            when 12
+              expect(card.to_s).must_equal "Queen of #{suit}"
+            when 13
+              expect(card.to_s).must_equal "King of #{suit}"
+          end
+        end
+      end
+
     end
   end
 
@@ -41,11 +66,38 @@ describe Card do
 
     it "Can retrieve the value of the card using a `.value`." do
       # ensure that `.value works as expected`
+      [:hearts, :spades, :clubs, :diamonds].each do |suit|
+        (1..13).each do |value|
+          card = Card.new(value, suit)
+          case value
+            when 1
+              expect(card.value).must_equal "Ace"
+            when 11
+              expect(card.value).must_equal "Jack"
+            when 12
+              expect(card.value).must_equal "Queen"
+            when 13
+              expect(card.value).must_equal "King"
+            else
+              expect(card.value).must_equal value
+          end
+        end
+      end
+
+      card = Card.new(13, :hearts)
+
+      expect(card.value).must_equal "King"
+
     end
 
     it "Can retrieve the value of the card using a `.suit`." do
       # ensure that `.suit works as expected returning the symbol of the suit`
-
+      [:hearts, :spades, :clubs, :diamonds].each do |suit|
+        (1..13).each do |value|
+          card = Card.new(value, suit)
+          expect(card.suit).must_equal suit
+        end
+      end
     end
   end
 
